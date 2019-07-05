cb73dd0075492083d430ceb7f5aec5e0b055da5c
simone.cappella1997@gmail.com <simone.cappella1997@gmail.com>
2019-07-05 17:14:25 +0200

    Controllo e gestione situazione con file già presente

--- SpringBootProject/src/main/java/it/project/SpringBootProject/Model/ConnectionAdapter.java
+++ SpringBootProject/src/main/java/it/project/SpringBootProject/Model/ConnectionAdapter.java
@@ -1,6 +1,7 @@
 package it.project.SpringBootProject.Model;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -27,49 +28,65 @@ public class ConnectionAdapter {
 	
 	public void estabilishConnection()
 	{
+		File f = new File("./csvfile.csv");
+		if(!f.exists())
+		{
 		try 
 		{	
-			URLConnection newConnection = new URL(url).openConnection();
-			InputStream in = newConnection.getInputStream();  //per leggere uno stream di byte
-			
+			//apre una connessione verso un url
+			URLConnection newConnection = new URL(url).openConnection(); 
+			//per leggere uno stream di byte
+			InputStream in = newConnection.getInputStream();  
 			 String data = "";
 			 String line = "";
 			 try {
+			   //per trasformare lo stream di byte in caratteri
 			   InputStreamReader isr = new InputStreamReader( in );
+			   //legge da uno stream di caratteri
 			   BufferedReader buf = new BufferedReader( isr );
 			  
 			   while ((line = buf.readLine()) != null) 
 			   {
+				   //in data ho l'intero JSON
 				   data+= line;
 			   }
 			 } finally 
 			 	{
 				 in.close();
 			 	}
+			//vengono parsati i dati json e castati in un oggetto json (chiave-valore)
 			JSONObject obj1 = (JSONObject) JSONValue.parseWithException(data); 
+			//ritorna i valori associati alla chiave result
 			JSONObject obj2 = (JSONObject) (obj1.get("result"));
+			//ritorna i valori associati alla chiave resources
 			JSONArray obj3 = (JSONArray) (obj2.get("resources"));
 			
 			for(Object o: obj3){
 			    if (o instanceof JSONObject) {
 			        JSONObject o1 = (JSONObject)o; 
+			        //prendo il valore della chiave format
 			        String format = (String)o1.get("format");
-			        String urlD = (String)o1.get("url");
-			        System.out.println(format + " | " + urlD);
+			        //prendo il valore della chiave url
+			        String urlCSV = (String)o1.get("url");
+			        //se il valore di format è quello che cerchiamo attiviamo la funzione download
 			        if(format.equals("http://publications.europa.eu/resource/authority/file-type/CSV"))
 			        {
-			        	download(urlD, "csvfile.csv");
+			        	download(urlCSV, "csvfile.csv");
 			        	result = "Download del file 'csvfile.csv' avvenuto correttamente, si troverà nella directory dell'applicazione.";
 			        	break;
 			        }
 			    }
 			}
-			System.out.println( "OK" );
-		} catch (IOException | ParseException e) {
+		} 	//nel try interno possono essere lanciate eccezioni di parsing o di IO
+			catch (IOException | ParseException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		}else 	
+			{
+				result = "Il file 'csvfile.csv è già presente nel tuo sistema, il download non è stato effettuato."; 
+			}
 	}
 	
 	@Override
@@ -81,6 +98,7 @@ public class ConnectionAdapter {
 	{
 	    try (InputStream in = URI.create(url).toURL().openStream()) 
 	    {
+	    	//copia il file nel path target
 	        Files.copy(in, Paths.get(nomefile));
 	    }
 	}
