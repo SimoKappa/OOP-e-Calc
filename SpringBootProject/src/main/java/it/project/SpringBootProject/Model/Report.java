ea6e1305e69bd2a6c3328ca9f32098b102a8a232
Danitoms <49287565+Danitoms@users.noreply.github.com>
2019-07-08 21:33:33 +0200

    piccole correzioni ai tipi e al report service

--- SpringBootProject/src/main/java/it/project/SpringBootProject/Model/Report.java
+++ SpringBootProject/src/main/java/it/project/SpringBootProject/Model/Report.java
@@ -5,17 +5,17 @@ public class Report {
 	private String refPeriod;
 	private String item;
 	private String code;
-	private String value;
-	private String extraction;
+	private double value;
+	private int extraction;
 	private int nca;
 	
-	public Report (String countryString, String refPeriodString, String itemString, String codeString, String valueString, String extractionString, int nca) {
+	public Report (String countryString, String refPeriodString, String itemString, String codeString, double value, int extraction, int nca) {
 		this.country = countryString;
 		this.refPeriod = refPeriodString;
 		this.item = itemString;
 		this.code = codeString;
-		this.value = valueString;
-		this.extraction = extractionString;
+		this.value = value;
+		this.extraction = extraction;
 		this.nca = nca;
 	}
 	
@@ -31,10 +31,10 @@ public class Report {
 	public String getCode() {
 		return code;
 	}
-	public String getValue() {
+	public double getValue() {
 		return value;
 	}
-	public String getExtraction() {
+	public int getExtraction() {
 		return extraction;
 	}
 	public int getNca() {
--- SpringBootProject/src/main/java/it/project/SpringBootProject/Service/ReportService.java
+++ SpringBootProject/src/main/java/it/project/SpringBootProject/Service/ReportService.java
@@ -1,6 +1,5 @@
 package it.project.SpringBootProject.Service;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -39,6 +38,8 @@ public class ReportService {
 				// System.out.println("country = " + lineStrings[0] + " " + lineStrings[1] + " "
 				// + lineStrings[2] + " " + lineStrings[3] + " " + lineStrings[4] + " " +
 				// lineStrings[5] + " " + lineStrings[6]);
+				//lineStrings[2].replaceAll("[\ufffd]", "");
+				lineStrings[2] = lineStrings[2].replaceAll("\uFFFD", "");
 				if (i != 0) {
 					createReport(lineStrings);
 				}
@@ -60,11 +61,11 @@ public class ReportService {
 		String refPeriodString = data[1];
 		String itemString = data[2];
 		String codeString = data[3];
-		String valueString = data[4];
-		String extractionString = data[5];
+		Double valueString = Double.parseDouble(data[4]);
+		int extraction = Integer.parseInt(data[5]);
 		int nca = Integer.parseInt(data[6]);
 		reports.add(
-				new Report(countryString, refPeriodString, itemString, codeString, valueString, extractionString, nca));
+				new Report(countryString, refPeriodString, itemString, codeString, valueString, extraction, nca));
 	}
 
 }
