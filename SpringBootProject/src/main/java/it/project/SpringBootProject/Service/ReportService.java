912d2e3fe33401c9aa457a05ba1369c38e53fa18
Danitoms <49287565+Danitoms@users.noreply.github.com>
2019-07-08 18:48:21 +0200

    utilizzata la libreria opencsv

--- SpringBootProject/src/main/java/it/project/SpringBootProject/Service/ReportService.java
+++ SpringBootProject/src/main/java/it/project/SpringBootProject/Service/ReportService.java
@@ -9,60 +9,53 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import au.com.bytecode.opencsv.CSVReader;
 import it.project.SpringBootProject.Model.Report;
 
 @Service
 public class ReportService {
 
 	private static List<Report> reports = new ArrayList<>();
-	
-	static
-	{
-		try
-		{
-		BufferedReader reader = new BufferedReader(new FileReader("csvfile.csv"));
-		try{
-		String line;
-		int i =0;
-		while ((line = reader.readLine())!=null) {
-			String [] data = line.split(",");
-			//System.out.println(data[0]);
-			if (i!=0) {
-				/*String countryString = data[0];
-				String refPeriodString = data[1];
-				String itemString = data[2];
-				String codeString = data[3];
-				//double value = Double.parseDouble(data[4]);
-				double value = 3;
-				int extraction = Integer.parseInt(data[5]);
-				int nca = Integer.parseInt(data[6]);
-				Report recordReport = new Report(countryString, refPeriodString, itemString, codeString, value, extraction, nca);
-				*/
-				createReport(data);
-				//Report recordReport = createReport(data);
-				//System.out.println(recordReport.toString());
+
+	static {
+		/*
+		 * try { BufferedReader reader = new BufferedReader(new
+		 * FileReader("csvfile.csv")); try{ String line; int i =0; while ((line =
+		 * reader.readLine())!=null) { String [] data = line.split(","); //divide la
+		 * stringa quando c'è la virgola for (String aStrings : data) {
+		 * aStrings.replaceAll("/\".*?\"/", ""); System.out.println(aStrings); } if
+		 * (i!=0) { createReport(data); } i++; } }catch(IOException e) {
+		 * e.printStackTrace(); } }catch (FileNotFoundException e) {
+		 * e.printStackTrace(); }
+		 */
+
+		String fileString = "csvfile.csv";
+		CSVReader reader = null;
+		try {
+			reader = new CSVReader(new FileReader(fileString));
+			String[] lineStrings;
+			int i = 0;
+			while ((lineStrings = reader.readNext()) != null) {
+				// System.out.println("country = " + lineStrings[0] + " " + lineStrings[1] + " "
+				// + lineStrings[2] + " " + lineStrings[3] + " " + lineStrings[4] + " " +
+				// lineStrings[5] + " " + lineStrings[6]);
+				if (i != 0) {
+					createReport(lineStrings);
 				}
-				//Report recoReport = createReport(attributeStrings);
-				//line = reader.readLine();
-			i++;
-		}
-		}catch(IOException e)
-		{
+				i++;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		}catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		
 	}
-	
-	public List<Report> retrieveallreports()
-	{
+
+	public List<Report> retrieveallreports() {
 		return reports;
 	}
-	
-	private static void createReport (String[] data) {
+
+	private static void createReport(String[] data) {
 		String countryString = data[0];
 		String refPeriodString = data[1];
 		String itemString = data[2];
@@ -70,8 +63,8 @@ public class ReportService {
 		String valueString = data[4];
 		String extractionString = data[5];
 		int nca = Integer.parseInt(data[6]);
-		//return new Report(countryString, refPeriodString, itemString, codeString, valueString, extractionString, nca);
-		reports.add(new Report(countryString, refPeriodString, itemString, codeString, valueString, extractionString, nca));
+		reports.add(
+				new Report(countryString, refPeriodString, itemString, codeString, valueString, extractionString, nca));
 	}
-	
+
 }
